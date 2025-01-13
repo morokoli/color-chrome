41c18f819250ed708ffef4a1926344ae5c69bd77
Oleksandr Molodtsov <alex.koitechs@gmail.com>
2025-01-13 12:17:20 +0200

    main.tsx some codestyle changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./v2/components/App.tsx"
-import "./v2/assets/css/global.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import App from './v2/components/App.tsx'
+
+import './v2/assets/css/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
