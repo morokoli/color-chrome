e314a7592340387055818468d44859c62401197d
korayfeyzula <koray.testing@gmail.com>
2025-12-08 02:48:36 +0200

    Remove deprecated v1 folder

--- src/v1/components/InputError.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react"
-
-type Props = {
-  message: string
-}
-
-export const InputError: FC<Props> = (props) => {
-  return <p className="text-xs text-red-700">{props.message}</p>
-}
--- src/v1/helpers/url.ts
+++
@@ -1,8 +0,0 @@
-export async function getPageURL(): Promise<string | undefined> {
-  if (!chrome.tabs) {
-    return window.location.href
-  }
-
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-  return tabs[0].url
-}
